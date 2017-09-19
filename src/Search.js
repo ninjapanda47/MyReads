3ea97b53917591c314336a0f961771aaf7bff59a
Sandi <sandi.c.gau@gmail.com>
2017-09-19 12:52:55 -0500

    added routes in App

---
+++ src/Search.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+// import * as BooksAPI from './BooksAPI'
+import { Route } from 'react-router-dom'
+import './App.css'
+
+class Search extends Component {
+
+  render() {
+    return (
+      <div className="search">
+      Search Page
+      </div>
+    )
+  }
+}
+
+export default Search
