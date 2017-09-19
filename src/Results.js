cf721d3ac3fcc8056da97b7100052ac01d96867b
Sandi <sandi.c.gau@gmail.com>
2017-09-19 13:21:36 -0500

    added Link to to all the links

--- src/Results.js
+++ src/Results.js
@@ -1,39 +1,17 @@
 import React, {Component} from 'react'
 // import * as BooksAPI from './BooksAPI'
 import { Route } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 import './App.css'
 
 class Results extends Component {
   state = {
-    showSearchPage: false
+
   }
 
   render() {
     return (
       <div className="results">
-
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-        ) : (
           <div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
@@ -190,7 +168,7 @@ class Results extends Component {
               </div>
             </div>
             <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+              <Link to ='/search' className="open-search"> Add a book</Link>
             </div>
           </div>
         )}
