702b9e2e33953149f611d7c5568f9498064617b4
Sandi <sandi.c.gau@gmail.com>
2017-09-23 15:40:59 -0500

    shelf filter works

--- src/Results.js
+++ src/Results.js
@@ -11,7 +11,6 @@ class Results extends Component {
 
     const { results } = this.props
     console.log(this.props)
-    const match = 'currentlyReading'
 
     return (
       <div className="results">
@@ -26,7 +25,7 @@ class Results extends Component {
                 <div className="bookshelf-books">
                   <ol className="books-grid">
                   
-                    {results.filter((result) => (result.shelf) == match).map((result) => (
+                    {results.filter((result) => (result.shelf) == 'currentlyReading').map((result) => (
                       <li key = { result.id } >
                         <div className="book-top">
                           <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${result.imageLinks.smallThumbnail})`}}></div>
@@ -51,11 +50,11 @@ class Results extends Component {
                 <h2 className="bookshelf-title">Want to Read</h2>
                 <div className="bookshelf-books">
                   <ol className="books-grid">
-                    <li>
-                      <div className="book">
+                    {results.filter((result) => (result.shelf) == 'wantToRead').map((result) => (
+                      <li key = { result.id } >
                         <div className="book-top">
-                          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api")' }}></div>
-                          <div className="book-shelf-changer">
+                          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${result.imageLinks.smallThumbnail})`}}></div>
+                          <div className="book-shelf-changer"> 
                             <select>
                               <option value="none" disabled>Move to...</option>
                               <option value="currentlyReading">Currently Reading</option>
@@ -65,10 +64,10 @@ class Results extends Component {
                             </select>
                           </div>
                         </div>
-                        <div className="book-title">1776</div>
-                        <div className="book-authors">David McCullough</div>
-                      </div>
-                    </li>
+                        <div className="book-title">{result.title}</div>
+                        <div className="book-authors">{result.authors}</div>
+                      </li>
+                        ))}
                   </ol>
                 </div>
               </div>
@@ -76,11 +75,11 @@ class Results extends Component {
                 <h2 className="bookshelf-title">Read</h2>
                 <div className="bookshelf-books">
                   <ol className="books-grid">
-                    <li>
-                      <div className="book">
+                    {results.filter((result) => (result.shelf) == 'read').map((result) => (
+                      <li key = { result.id } >
                         <div className="book-top">
-                          <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: 'url("http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api")' }}></div>
-                          <div className="book-shelf-changer">
+                          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${result.imageLinks.smallThumbnail})`}}></div>
+                          <div className="book-shelf-changer"> 
                             <select>
                               <option value="none" disabled>Move to...</option>
                               <option value="currentlyReading">Currently Reading</option>
@@ -90,20 +89,20 @@ class Results extends Component {
                             </select>
                           </div>
                         </div>
-                        <div className="book-title">The Hobbit</div>
-                        <div className="book-authors">J.R.R. Tolkien</div>
-                      </div>
-                    </li>
+                        <div className="book-title">{result.title}</div>
+                        <div className="book-authors">{result.authors}</div>
+                      </li>
+                        ))}
                   </ol>
                 </div>
               </div>
             </div>
           </div>
           <div className="open-search">
-            <Link to='/' className="open-search"> Add a book</Link>
+            <Link to='/search' className="open-search"> Add a book</Link>
           </div>
         </div>
-        )}
+        
       </div>
     )
   }
